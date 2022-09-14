6e8a244e0a8dea55e1f2e54e55ea74f251e0228c
Ryan Yogan <ryan@ryans-mbp.lan>
2022-09-13 21:11:40 -0500

    save a user on login

---
+++ models/user.js
@@ -0,0 +1,12 @@
+import { firestore } from "../lib/firebase";
+
+export function updateUser(uid, data) {
+  return firestore.collection("users").doc(uid).update(data);
+}
+
+export function createUser(uid, data) {
+  return firestore
+    .collection("users")
+    .doc(uid)
+    .set({ uid, ...data }, { merge: true });
+}
