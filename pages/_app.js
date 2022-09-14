f2663036f4aa06bd19667db7011dd1b44072c8ab
Ryan Yogan <ryan@ryans-mbp.lan>
2022-09-14 10:42:04 -0500

    some shit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
 import { CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { css, Global } from "@emotion/core";
+import Head from "next/head";
 import { AuthProvider } from "../lib/auth";
+
 import theme from "../styles/theme";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <CSSReset />
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ThemeProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
+import { Button, Code, Heading, Text } from "@chakra-ui/core";
+import Head from "next/head";
+
 import { useAuth } from "../lib/auth";
 
-export default function Home() {
+export default function Index() {
   const auth = useAuth();
 
-  return auth.user ? (
-    <div>
-      <p>Email: {auth.user.email}</p>
-      <button onClick={(e) => auth.signout()}>Sign out</button>
+  return (
+    <div className="container">
+      <Head>
+        <title>Fast Feedback (React 2025)</title>
+      </Head>
+
+      <main>
+        <Heading>Fast Feedback</Heading>
+
+        <Text>
+          Current User: <Code>{auth.user ? auth.user.email : "None"}</Code>
+        </Text>
+        {auth.user ? (
+          <div>
+            <p>Email: {auth.user.email}</p>
+            <Button onClick={(e) => auth.signout()}>Sign out</Button>
+          </div>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
+        )}
+      </main>
     </div>
-  ) : (
-    <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
   );
 }
